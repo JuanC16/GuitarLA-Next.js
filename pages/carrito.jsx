bfb61af3669a6a82e7768fce63c3a26e0694bb95
JuanC16 <juancarlosdazaquiroga17@gmail.com>
2023-05-25 22:30:37 -0500

    Corrección de error 3

--- pages/carrito.jsx
+++ pages/carrito.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import {useState, useEffect} from "react"
 import Image from "next/image"
 import Layout from "../components/layout"
 import styles from "../styles/carrito.module.css"
